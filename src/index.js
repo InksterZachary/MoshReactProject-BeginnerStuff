df12ef3a747ee7cad323163bc736b9e04fee8560
Zachary Inkster <z.j.inkster@gmail.com>
2021-03-12 13:56:40 -0600

    Incorporating the use of JS functions as objects to reference the object we want to use.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ const person = {
 };
 //calls function from object
 person.walk();
+
 //sets walk to person function
-const walk = person.walk;
+// const walk = person.walk;
+
+//by binding the object to the js function we are able to reference the object the way we want to
+const walk = person.walk.bind(person);
+
 //strict mode enabled by default so this will return undefine. It does not reference the window object
 walk();
