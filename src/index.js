c9237b1fe90b17f4e5355c06c92011534fa2803a
Zachary Inkster <z.j.inkster@gmail.com>
2021-03-12 13:32:57 -0600

    Understanding how objects are referenced

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ const person = {
         console.log(this);
     }
 };
-
+//calls function from object
 person.walk();
+//sets walk to person function
+const walk = person.walk;
+//strict mode enabled by default so this will return undefine. It does not reference the window object
+walk();
